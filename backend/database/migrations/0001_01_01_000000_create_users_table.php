b286d7261d07ba872443767e594c3331bf416124
Hasan-Atwi <161140678+Hasan-Atwi@users.noreply.github.com>
2025-05-21 11:49:53 +0300

    added hashed passwords to DB

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,12 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamp('created_at')->useCurrent();
+            $table->string('password'); // Will store hashed passwords
+            $table->rememberToken(); // Crucial for "remember me" functionality
+            $table->timestamps(); // Adds both created_at AND updated_at
         });
-
-
-
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
@@ -37,7 +35,6 @@ return new class extends Migration
             $table->integer('last_activity')->index();
         });
     }
-
     /**
      * Reverse the migrations.
      */
